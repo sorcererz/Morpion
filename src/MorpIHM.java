6b795777077cfbc95285e349df8afd9c0c61596f
Thomas Buvat <buvat.thomas.pro@gmail.com>
2019-09-24 15:02:45 +0200

    display grid update

--- src/MorpIHM.java
+++ src/MorpIHM.java
@@ -11,9 +11,14 @@ public class MorpIHM {
     }
 	
 	public void displayGrid(String case1,String case2,String case3,String case4,String case5,String case6,String case7,String case8,String case9) {
-		display(case1 + " " + case2 + " " + case3);
-		display(case4 + " " + case5 + " " + case6);
-		display(case7 + " " + case8 + " " + case9);
+		display("\u250F" + "\u2501" + "\u2501" + "\u2501" + "\u2533" + "\u2501" + "\u2501" + "\u2501" + "\u2533" + "\u2501" + "\u2501" + "\u2501" + "\u2513");
+		display("\u2503" + " " + case1 + " " + "\u2503" + " "+ case2 + " " + "\u2503" + " "+ case3 + " " + "\u2503");
+		display("\u2523" + "\u2501" + "\u2501" + "\u2501" + "\u254B" + "\u2501" + "\u2501" + "\u2501" + "\u254B" + "\u2501" + "\u2501" + "\u2501" + "\u252B");
+		display("\u2503" + " " + case4 + " " + "\u2503" + " "+ case5 + " " + "\u2503" + " "+ case6 + " " + "\u2503");
+		display("\u2523" + "\u2501" + "\u2501" + "\u2501" + "\u254B" + "\u2501" + "\u2501" + "\u2501" + "\u254B" + "\u2501" + "\u2501" + "\u2501" + "\u252B");
+		display("\u2503" + " " + case7 + " " + "\u2503" + " "+ case8 + " " + "\u2503" + " "+ case9 + " " + "\u2503");
+		display("\u2517" + "\u2501" + "\u2501" + "\u2501" + "\u253B" + "\u2501" + "\u2501" + "\u2501" + "\u253B" + "\u2501" + "\u2501" + "\u2501" + "\u251B");
+
 	}
 	
 	public String read() {
